9c7c344d3b92ca44d109cf0528e89d2489f77081
KaniyaHamilton <kaniyahmilton08@gmail.com>
2024-11-19 21:32:56 -0500

    resubmission

--- quote.js
+++ quote.js
@@ -5,6 +5,12 @@ const divFloors = document.getElementById("Number-of-floors")
 const divOccupancy = document.getElementById("maximum-Occupancy")
 const divElevators = document.getElementById("Number-of-Elevators")
 
+//THE CONTAINERS FOR THE BOTTOM 4 INPUT FIELDS
+const divelevatorsneededoutput = document.getElementById ("Elevators-Required")
+const divtotalcost = document.getElementById ("Total-Cost")
+const divInstillationOutput = document.getElementById ("instillation-fees")
+const divunitcost = document.getElementById ("CPU")
+
 //BUILDING TYPE RADIO BUTTONS
 const Resedentialbutton = document.getElementById('Resedential')
 const Commercialbutton = document.getElementById('Commercial')
@@ -39,7 +45,11 @@ divFloors.style.display = "none"
 divOccupancy.style.display = "none"
 divElevators.style.display = "none"
 
-
+//TURNING OFF THE DISPLAY FOR THE BOTTOM 4 INPUT CONTAINERS
+divelevatorsneededoutput.style.display = "none"
+divtotalcost.style.display = "none"
+divInstillationOutput.style.display = "none"
+divunitcost.style.display = "none"
 
 //RESIDENTIAL  COMMERCIAL  INDUSTRIAL BUTTONS
 Resedentialbutton.addEventListener("click", () => {
@@ -51,7 +61,15 @@ Resedentialbutton.addEventListener("click", () => {
     divOccupancy.style.display = "none"
     
     divElevators.style.display = "none"
+    
 
+    divelevatorsneededoutput.style.display = "block"
+
+    divtotalcost.style.display = "block"
+
+    divInstillationOutput.style.display = "block"
+
+    divunitcost.style.display = "block"
 })
 
 //FUNCTIONAL
@@ -65,6 +83,14 @@ Commercialbutton.addEventListener("click", () => {
     
     divElevators.style.display = "none"
 
+
+    divelevatorsneededoutput.style.display = "block"
+
+    divtotalcost.style.display = "block"
+
+    divInstillationOutput.style.display = "block"
+
+    divunitcost.style.display = "block"
 })
 
 //FUNCTIONAL
@@ -77,15 +103,19 @@ Industrialbutton.addEventListener("click", () => {
     divOccupancy.style.display = "none"
     
     divElevators.style.display = "block"
+ 
+    
+    divelevatorsneededoutput.style.display = "block"
 
+    divtotalcost.style.display = "block"
+
+    divInstillationOutput.style.display = "block"
+
+    divunitcost.style.display = "block"
 })
 
 
-
-//CALCULATING COSTS
-apartmentinput.addEventListener("input", () => {
-    console.log("hello")
-
+const residentialMath = () => {
     const averageapartments = Math.ceil(apartmentinput.value / floorsinput.value)
     console.log(averageapartments)
     const requiredelevators = Math.ceil(averageapartments/6)
@@ -97,12 +127,67 @@ apartmentinput.addEventListener("input", () => {
     console.log(elevatorsneeded)
     
     elevatorsneededoutput.value = elevatorsneeded
+}
+
+
+//CALCULATING COSTS
+apartmentinput.addEventListener("input", () => {
+    if (apartmentinput.value < 0) {
+         return apartmentinput.value = 0; 
+    }
+
+    residentialMath()
+
+    if(standardPPE.checked){
+        standardMath()
+    }
+    if(premiumPPE.checked){
+        premiumMath()
+    }
+    if(exceliumPPE.checked){
+        exceliumMath()
+    }
+})
+
+floorsinput.addEventListener("input", () => {
+    if (floorsinput.value < 0) {
+         return floorsinput.value = 0; 
+    }
+
+    if(Commercialbutton.checked){
+        commercialMath()
+    }else if(Resedentialbutton.checked){
+        residentialMath() 
+    }
+    if(standardPPE.checked){
+        standardMath()
+    }
+    if(premiumPPE.checked){
+        premiumMath()
+    }
+    if(exceliumPPE.checked){
+        exceliumMath()
+    }
+})
+
+Maximumoccupancy.addEventListener("input", () => {
+    if (Maximumoccupancy.value < 0) {
+         return Maximumoccupancy.value = 0; 
+    }
+    commercialMath()
+    if(standardPPE.checked){
+        standardMath()
+    }
+    if(premiumPPE.checked){
+        premiumMath()
+    }
+    if(exceliumPPE.checked){
+        exceliumMath()
+    }
 })
 
 
-//FUNCTIONAL
-Maximumoccupancy.addEventListener("input", () => {
-    console.log("hello")
+const commercialMath = () => {
     
     const totaloccupants = Maximumoccupancy.value * floorsinput.value
     
@@ -115,6 +200,15 @@ Maximumoccupancy.addEventListener("input", () => {
     console.log(elevatorsneeded)
     
     elevatorsneededoutput.value = elevatorsneeded
+}
+
+//FUNCTIONAL
+Maximumoccupancy.addEventListener("input", () => {
+    if (Maximumoccupancy.value < 0) {
+        return Maximumoccupancy.value = 0; 
+   }
+    
+    commercialMath()
 })
 
 
@@ -127,43 +221,119 @@ const industrialelevators = document.getElementById ("NUEInput")
 industrialelevators.addEventListener("input", () => {
 
     elevatorsneededoutput.value = industrialelevators.value
-
+    if(standardPPE.checked){
+        standardMath()
+    }
+    if(premiumPPE.checked){
+        premiumMath()
+    }
+    if(exceliumPPE.checked){
+        exceliumMath()
+    }
 
 })
 
 
-    
+function formatCurrency(amount, locale = 'en-US', currency = 'USD') {
+    return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currency
+}).format(amount);
+}
 
 //STANDARD  PREMIUM  EXCELIUM BUTTONS
-
-standardPPE.addEventListener("click", () => {
-    //unit cost
-   //instillation cost
-
-
-    const unitcostperelevator = 8000
-    const InstillationCost = 0.10
+const exceliumMath = () => {
+    const unitcostperelevator = 15000
+    const InstillationCost = 0.20
 
     //Elevators required
+const elevatorsrequired = elevatorsneededoutput.value
+
+const formattedUnitCost = formatCurrency(unitcostperelevator)
+//declaring the unit cost, pre instillation cost, and instillation fee container 
+unitcost.value = formattedUnitCost
+
+const preinstillationcost = unitcostperelevator * elevatorsrequired
+
+const instillationfeecontainer = preinstillationcost * InstillationCost
+
+const formattedInstillationFee = formatCurrency(instillationfeecontainer)
+//instillation fees
+
+InstillationOutput.value = formattedInstillationFee
+
+//final cost
+const finalcost = (preinstillationcost + instillationfeecontainer)
+
+//multiplying the total cost by the final cost
+
+const formattedCurrency = formatCurrency(finalcost)
+
+ totalcost.value = formattedCurrency
+}
+
+const premiumMath = () => {
+    const unitcostperelevator = 12000
+        const InstillationCost = 0.15
+
+        //Elevators required
     const elevatorsrequired = elevatorsneededoutput.value
 
-    //declaring the unit cost, pre instillation cost, and instillation fee container  
-    unitcost.value = unitcostperelevator
+    const formattedUnitCost = formatCurrency(unitcostperelevator)
+       //declaring the unit cost, pre instillation cost, and instillation fee container 
+    unitcost.value = formattedUnitCost
 
     const preinstillationcost = unitcostperelevator * elevatorsrequired
 
     const instillationfeecontainer = preinstillationcost * InstillationCost
-    
+
+    const formattedInstillationFee = formatCurrency(instillationfeecontainer)
     //instillation fees
-    InstillationOutput.value = instillationfeecontainer
+    InstillationOutput.value = formattedInstillationFee
 
     //final cost
     const finalcost = (preinstillationcost + instillationfeecontainer)
     
     //multiplying the total cost by the final cost
 
-     totalcost.value = finalcost
+    const formattedCurrency = formatCurrency(finalcost)
 
+     totalcost.value = formattedCurrency
+}
+const standardMath = () => {
+    const unitcostperelevator = 8000
+    const InstillationCost = 0.10
+
+    //Elevators required
+    const elevatorsrequired = elevatorsneededoutput.value
+
+    const formattedUnitCost = formatCurrency(unitcostperelevator)
+
+    //declaring the unit cost, pre instillation cost, and instillation fee container  
+    unitcost.value = formattedUnitCost
+
+    const preinstillationcost = unitcostperelevator * elevatorsrequired
+
+    const instillationfeecontainer = preinstillationcost * InstillationCost
+
+    const formattedInstillationFee = formatCurrency(instillationfeecontainer)
+    
+    //instillation fees
+    InstillationOutput.value = formattedInstillationFee 
+
+    //final cost
+    const finalcost = (preinstillationcost + instillationfeecontainer)
+    
+    //multiplying the total cost by the final cost
+    
+
+    const formattedCurrency = formatCurrency(finalcost)
+
+    totalcost.value = formattedCurrency
+}
+
+standardPPE.addEventListener("click", () => {
+    standardMath()
 })
 
 
@@ -171,62 +341,11 @@ standardPPE.addEventListener("click", () => {
     
 
     premiumPPE.addEventListener("click", () => {
-        //unit cost
-        //instillation cost
-
-
-        const unitcostperelevator = 12000
-        const InstillationCost = 0.15
-
-        //Elevators required
-    const elevatorsrequired = elevatorsneededoutput.value
-
-       //declaring the unit cost, pre instillation cost, and instillation fee container 
-    unitcost.value = unitcostperelevator
-
-    const preinstillationcost = unitcostperelevator * elevatorsrequired
-
-    const instillationfeecontainer = preinstillationcost * InstillationCost
-
-    //instillation fees
-    InstillationOutput.value = instillationfeecontainer
-
-    //final cost
-    const finalcost = (preinstillationcost + instillationfeecontainer)
-    
-    //multiplying the total cost by the final cost
-
-     totalcost.value = finalcost
+     premiumMath()
     })
     
 
     
     exceliumPPE.addEventListener("click", () => {
-        //unit cost
-        //instillation cost
-
-
-        const unitcostperelevator = 15000
-        const InstillationCost = 0.20
-
-        //Elevators required
-    const elevatorsrequired = elevatorsneededoutput.value
-
-    //declaring the unit cost, pre instillation cost, and instillation fee container 
-    unitcost.value = unitcostperelevator
-
-    const preinstillationcost = unitcostperelevator * elevatorsrequired
-
-    const instillationfeecontainer = preinstillationcost * InstillationCost
-
-    //instillation fees
-    InstillationOutput.value = instillationfeecontainer
-    
-    //final cost
-    const finalcost = (preinstillationcost + instillationfeecontainer)
-    
-    //multiplying the total cost by the final cost
-
-     totalcost.value = finalcost
-
-    })
+    exceliumMath()
+})
